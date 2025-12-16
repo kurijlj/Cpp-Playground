e6b284d1d86bc69bb28753dda84b344873829400
Ljubomir Kurij <ljubomir_kurij@protonmail.com>
2025-12-16 06:59:12 +0100

    Added gaussian trajectory generator.

--- src/binning_study.cxx
+++ src/binning_study.cxx
@@ -9,28 +9,67 @@
 #include <string>
 #include <vector>
 
+// constexpr float pi = 3.14159;
+
 using String = std::string;
 using Function = std::vector<float>;
 using Iterator = Function::iterator;
 
 LogService::ConsoleLogService binning_log{"BINNING_STUDY"};
 
-struct SineMovementGenerator {
-	using Iterator = std::vector<float>::iterator;
+class SineTrajectoryGenerator {
+public:
+	SineTrajectoryGenerator(const float& equilibrium, const float& amplitude)
+		: equilibrium_{equilibrium}, amplitude_{amplitude} { }
 
 	void operator()(const Iterator& begin, const Iterator& end) {
 		for (Iterator current(begin); current != end; current++) {
 			size_t index{current - begin};
-			float position = 3.14 * static_cast<float> (index) / 180.0;
-			*current = 360.0 * std::sin(position);
+			float radians = 3.14 * static_cast<float> (index) / 180.0;
+			*current = 2 * amplitude_ * std::sin(radians) + equilibrium_;
+		}
+	}
+
+private:
+	const float equilibrium_;
+	const float amplitude_;
+};
+
+class FidTrajectoryGenerator {
+public:
+	void operator()(const Iterator& begin, const Iterator& end) {
+		for (Iterator current(begin); current != end; current++) {
 		}
 	}
 };
 
-class BinToWidth {
+class GaussianTrajectoryGenerator {
 public:
-	BinToWidth() = default;
-	explicit BinToWidth(const float& width) {
+	GaussianTrajectoryGenerator(
+		const float& amplitude,
+		const float& mu,
+		const float& sigma
+	) : amplitude_{amplitude}, mu_{mu}, sigma_{sigma} { }
+	void operator()(const Iterator& begin, const Iterator& end) {
+		for (Iterator current(begin); current != end; current++) {
+			size_t index{current - begin};
+			*current = amplitude_ * std::exp(
+				-(std::pow(index - mu_, 2))
+					/ (2 * std::pow(sigma_, 2))
+			);
+		}
+	}
+
+private:
+	const float amplitude_;
+	const float mu_;
+	const float sigma_;
+};
+
+class BinBySignalStep {
+public:
+	BinBySignalStep() = default;
+	explicit BinBySignalStep(const float& width) {
 		if (1.0 > width) {
 			binning_log.Error(
 				String{"Bin width too small. "}
@@ -101,13 +140,14 @@ int main(int argc, char* argv[]) {
 	constexpr size_t data_points{360};
     Function f1(data_points, 0);
     Function f1binned(data_points, 0);
-	SineMovementGenerator makeSineMovement{};
-	BinToWidth binToOneDegree{10.0};
+	SineTrajectoryGenerator makeSineTrajectory{180.0, 180.0};
+	GaussianTrajectoryGenerator makeGaussianTrajectory{360.0, 180.0, 45.0};
+	BinBySignalStep binByTen{10.0};
 
 	binning_log.Info("Starting ...");
 
-	makeSineMovement(f1.begin(), f1.end());
-	binToOneDegree(
+	makeGaussianTrajectory(f1.begin(), f1.end());
+	binByTen(
 		f1.begin(),
 		f1.end(),
 		f1binned.begin()
